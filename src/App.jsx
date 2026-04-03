f34cb294f91591099ba8614feca2ea60d5b606a3
johntez38 <mureithijohn117@gmail.com>
2026-04-03 15:34:42 +0300

    Add App component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+
+import React from 'react';
+import './index.css';
+
+const App = () => {
+    return (
+        <div>
+            <h1>DashboardGPS</h1>
+        </div>
+    );
+};
+
+export default App;
